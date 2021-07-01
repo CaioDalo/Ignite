12cadba5d0a46cd081bf9ba4b4cfa3c2547aded9
Caio Dalo <caiodalo93@gmail.com>
2021-07-01 20:59:23 -0300

    criado estilo para a listagem de repositorios e criado a requisição para o github

--- ReactJS/01-github-explorer/src/components/RepositoryList.jsx
+++ ReactJS/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react"
 import { RepositoryItem } from "./RepositoryItem"
+
+import '../styles/repositories.scss'
 
 const repository = {
     name:'unform2', 
@@ -7,6 +10,17 @@ const repository = {
 }
 
 export function RepositoryList() {
+
+    const urlApi = 'https://api.github.com/orgs/rocketseat/repos'
+
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        fetch(urlApi)
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [])
+
     return (
         <section className='repository-list'>
             <h1>Lista de repositórios</h1>
