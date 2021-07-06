e75fe8751128a7d66cdc48c803e070742c65ccdd
Caio Dalo <caiodalo93@gmail.com>
2021-07-05 21:00:28 -0300

    configurado todos os arquivos para .tsx

--- ReactJS/01-github-explorer/src/components/RepositoryList.tsx
+++ ReactJS/01-github-explorer/src/components/RepositoryList.tsx
@@ -3,11 +3,17 @@ import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
 
+interface Repository {
+    name: string
+    description: string
+    html_url: string
+}
+
 export function RepositoryList() {
 
     const urlApi = 'https://api.github.com/orgs/rocketseat/repos'
 
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
         fetch(urlApi)
