95e02ea3e23d6c24fdb6a3e8d77aaf07e6fe9bcf
Caio Dalo <caiodalo93@gmail.com>
2021-09-01 20:06:58 -0300

    adicionado axios, miragejs e react-modal para clicque do botão adicionar nova transação

--- ReactJS/dtmoney/src/App.tsx
+++ ReactJS/dtmoney/src/App.tsx
@@ -1,13 +1,38 @@
+import { useState } from 'react'
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import Modal from 'react-modal'
 
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
+
+  const [isTransactionNewModalOpen, setIsTransactionNewModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+      setIsTransactionNewModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsTransactionNewModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+      <Modal 
+      isOpen={isTransactionNewModalOpen} 
+      onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
