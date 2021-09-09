b33e083631f6b7d936711f04dd9ef0be1af72cfd
Caio Dalo <caiodalo93@gmail.com>
2021-09-09 20:30:25 -0300

    Criado context para transactions

--- ReactJS/dtmoney/src/App.tsx
+++ ReactJS/dtmoney/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import Modal from 'react-modal'
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import Modal from 'react-modal'
 import { NewTrasactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext'
 
 import { GlobalStyle } from "./styles/global";
 
@@ -22,7 +23,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 
       <Dashboard />
@@ -33,6 +34,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
