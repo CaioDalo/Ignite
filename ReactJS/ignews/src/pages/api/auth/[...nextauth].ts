fb820a927f272b68ff361e6da0925ac758b38266
Caio Dalo <caiodalo93@gmail.com>
2023-02-01 22:33:37 -0300

    feat: fauna db

--- ReactJS/ignews/src/pages/api/auth/[...nextauth].ts
+++ ReactJS/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,49 @@
+import { fauna } from '../../../services/fauna'
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export default NextAuth({
-  // Configure one or more authentication providers
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
         }),
     ],
+    callbacks: {
+        async signIn({user, account, profile}) {
+            const {email} = user
+
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('users_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('users_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+                return true
+
+            } catch (error) {
+                console.error(error)
+                return false
+            }
+        },
+    }
 })
