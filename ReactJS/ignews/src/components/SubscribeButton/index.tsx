583b806a58c4771c0e21774966c5f25ba0a0e09e
Caio Dalo <caiodalo93@gmail.com>
2023-03-01 20:55:08 -0300

    feat: stripe

--- ReactJS/ignews/src/components/SubscribeButton/index.tsx
+++ ReactJS/ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { getStripeJs } from '../../services/stripe-js'
+import { api } from '../../services/api'
+import { useSession, signIn } from "next-auth/react"
+
+
 import styles from "./styles.module.scss"
 
 interface SubscribeButtonProps {
@@ -5,9 +10,35 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    const session = useSession()
+
+    async function handleSubscribe() {
+        if(!session) {
+            return signIn('github')
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId: sessionId})
+
+        } catch (error) {
+            alert(error.message)
+            return
+        }
+    }
+
     return (
-        <button type="button" className={styles.subscribeButton}>
+        <button type="button" 
+            className={styles.subscribeButton}
+            onClick={handleSubscribe}
+        >
             Subscribe now
         </button>
     )
 }
+
