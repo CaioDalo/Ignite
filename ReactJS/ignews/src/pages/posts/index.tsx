515ca173c1b41afc2de95f938427af739939d759
Caio Dalo <caiodalo93@gmail.com>
2023-04-05 21:43:14 -0300

    feat: posts page

---
+++ ReactJS/ignews/src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styles from './styles.module.scss'
+
+export default function Posts() {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Prisma: uma das melhores coisas que já aconteceu no ecossistema?</strong>
+                        <p>Um breve arquivo sobre a origem da ferramenta que facilitou o acesso de databases e ampliou produtividade para o ecossistema JavaScript/TypeScript</p>
+                    </a>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Prisma: uma das melhores coisas que já aconteceu no ecossistema?</strong>
+                        <p>Um breve arquivo sobre a origem da ferramenta que facilitou o acesso de databases e ampliou produtividade para o ecossistema JavaScript/TypeScript</p>
+                    </a>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Prisma: uma das melhores coisas que já aconteceu no ecossistema?</strong>
+                        <p>Um breve arquivo sobre a origem da ferramenta que facilitou o acesso de databases e ampliou produtividade para o ecossistema JavaScript/TypeScript</p>
+                    </a>
+                </div>
+            </main>
+        </>
+    )
+}
